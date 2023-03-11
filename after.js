c3c04a1492f4a67beec8e5c64954243eb86d9766
thrutol <127605506+thrutol@users.noreply.github.com>
2023-03-11 21:21:13 +0800

    Add files via upload

---
+++ after.js
@@ -0,0 +1,8 @@
+document.write('</in>\
+  <div class="bg-n H-25 txt-w">\
+   <b>后室TH版</b>\
+   <pre><b class="ta-c">\
+    <a href="index.html">主页</a>\
+   </b></pre>\
+  </div>')
+document.getElementById("showtitle").innerHTML = document.getElementById("title").innerHTML;
---
+++ before.js
@@ -0,0 +1,17 @@
+document.write('\
+  <style>\
+   @import url("css/global.css");\
+   @import url("css/tool.css");\
+   @import url("css/color.css");\
+  </style>\
+  <label>≡</label>\
+   <aside>\
+    <header><a href="index.html">主页</a></header>\
+    <option><a href="notice.html">公告</a></option>\
+    <option><a href="mod.html">模组</a></option>\
+   </aside>\
+  <div class="bg-n H-25 txt-w">\
+   <p><b id="showtitle" style="font-size:30px;">null</b></p>\
+   \
+   <pre><b style="font-size:20px;">    后室TH版</b></pre>\
+  </div><in class="H-50">');
