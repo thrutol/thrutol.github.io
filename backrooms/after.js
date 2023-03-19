0e6ec017f16c755e22891456b6591feb592227fe
thrutol <127605506+thrutol@users.noreply.github.com>
2023-03-19 16:58:06 +0800

    Add files via upload

---
+++ backrooms/after.js
@@ -0,0 +1,9 @@
+document.write('<br<br></in>\
+  <t class="H-25 txt-w" id="foot">\
+   <b>后室TH版</b>\
+   <pre><b class="ta-c">\
+    <a href="index.html">主页</a>\
+   </b></pre>\
+  </t>\
+  </body></html>')
+document.getElementById("showtitle").innerHTML = " " + document.getElementById("title").innerHTML;
---
+++ backrooms/before.js
@@ -0,0 +1,21 @@
+document.write('\
+  <html><head>\
+  <style>\
+   @import url("../css/global.css");\
+   @import url("../css/tool.css");\
+   @import url("../css/color.css");\
+   @import url("../css/default.css");\
+   @import url("../css/layout.css");\
+  </style></head>\
+  <label>≡</label>\
+   <aside>\
+    <header><a href="../0/index.html">主页</a></header>\
+    <header><a href="../0/notice.html">公告</a></header>\
+    <button class="option"><a href="../0/normal-level.html">原创层级</a></button>\
+    <button class="option"><a href="../0/normal-entity.html">原创实体</a></button>\
+    <button class="option"><a href="../0/normal-object.html">原创物品</a></button>\
+   </aside>\
+  <t class="H-25 txt-w" id="head"><br>\
+   <p><b id="showtitle" style="font-size:5vh;"class="ws-w">null</b></p>\
+   <pre><b style="font-size:4vh;" id="subtitle">    后室TH版</b></pre>\
+  </t><in class="txt-w M-2" style="min-height:50%;" id="content">');
