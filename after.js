f7fc9deb8d58b381fd9caa1b6e753dee90117150
thrutol <127605506+thrutol@users.noreply.github.com>
2023-03-19 16:36:17 +0800

    Delete after.js

--- after.js
+++
@@ -1,8 +0,0 @@
-document.write('<br<br></in>\
-  <div class="bg-n H-25 txt-w">\
-   <b>后室TH版</b>\
-   <pre><b class="ta-c">\
-    <a href="index.html">主页</a>\
-   </b></pre>\
-  </div></body></html>')
-document.getElementById("showtitle").innerHTML = document.getElementById("title").innerHTML;
